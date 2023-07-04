af010807be775f1b4509764e2ddb425fb832ff49
lorenzo-ibay <lorenzo.ibay@gmail.com>
2023-07-04 22:37:39 +0800

    api complete

--- crudtastic-api/index.php
+++ crudtastic-api/index.php
@@ -13,3 +13,12 @@ $path = "./database.db";
 //     '
 // INSERT INTO ' . $tableName . '(entry) VALUES("' . $entry . '");'
 // );
+
+$rows = selectFromDB($path, "SELECT * FROM $tableName");
+$entries= [];
+if (count($rows)>0){
+    foreach($rows as $row => $value){
+        array_push($entries, $value);
+    }
+    echo json_encode($entries);
+}
